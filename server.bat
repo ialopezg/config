f989f4c86e0a9174951e2b784b99a3978486739e
Isidro A. López G <me@ialopezg.com>
2020-09-13 16:51:35 -0500

    Added local server host support

---
+++ server.bat
@@ -0,0 +1,3 @@
+printf "Welcome to Web Development Server Edition\n\n"
+
+php -S 127.0.0.1:8000
---
+++ server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+printf "Welcome to Web Development Server Edition\n\n"
+
+php -S 127.0.0.1:8000
